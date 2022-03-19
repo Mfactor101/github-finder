debc85fbd3d58d9d41a6f87b6e8f2dd0c09cea93
Mfactor101 <muheez842@gmail.com>
2022-03-19 10:38:17 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1> 
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar/>
+        
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/notfound' element={<NotFound/>}/>
+            <Route path='/*' element={<NotFound/>}/>
+          </Routes>
+        </main>
+
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
