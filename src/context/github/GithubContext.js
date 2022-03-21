331828fd2646ebb400dff4918c15e1f76f8b14b9
Mfactor101 <muheez842@gmail.com>
2022-03-21 21:13:39 +0000

    Children to children change

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -6,7 +6,7 @@ const GithubContext = createContext()
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-export const GithubProvider = ({Children}) => {
+export const GithubProvider = ({children}) => {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(true)
 
@@ -31,7 +31,7 @@ export const GithubProvider = ({Children}) => {
                 fetchUsers
             }}
             >
-                {Children}
+            {children}
         </GithubContext.Provider>    
     )
 }
